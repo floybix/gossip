43677da5038ecc6061e6406269b7799c531f1622
Felix Andrews <felix@nfrac.org>
2016-07-14 15:07:09 +0800

    rewrite with datascript.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject gossip "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Gossip simulator"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]]
+                 [datascript "0.15.1"]]
   :main ^:skip-aot gossip.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/gossip/core.clj
+++ src/gossip/core.clj
@@ -1,168 +1,245 @@
 (ns gossip.core
   (:gen-class)
   (:require [clojure.set :as set]
-            [clojure.core.match :refer [match]]))
+            [datascript.core :as d]))
 
-(defn empty-state
-  "The whole game state is a map with keys for each character, storing
-  their personal beliefs. Each character's beliefs are structured like
+(def db-schema
+  {:person/id {:db/doc "A character's keyword id, used in beliefs."
+               :db/cardinality :db.cardinality/one
+               :db/unique :db.unique/value}
+   :person/gender {:db/doc "A character's sex, :female or :male"
+                   :db/cardinality :db.cardinality/one}
+   :belief/mind {:db/doc "Which character's mind this belief is held in."
+                 :db/cardinality :db.cardinality/one}
+   :belief/cause {:db/doc "Which other belief led to this belief."
+                  :db/cardinality :db.cardinality/one
+                  :db/valueType :db.type/ref}
+   :belief/source {:db/doc "Who did I learn this from."
+                   :db/cardinality :db.cardinality/one}
+   :belief/thought {:db/doc "An english phrase expressing this belief/feeling."
+                    :db/cardinality :db.cardinality/one}
+   :relation/subject {:db/doc "Which character is the subject of this relationship, eg the angry one."
+                      :db/cardinality :db.cardinality/one}
+   :relation/object {:db/doc "Which character is the object of this relationship, eg the one angry with."
+                     :db/cardinality :db.cardinality/one}
+   :relation/feeling {:db/doc "What is the feeling of this relationship, eg anger."
+                      :db/cardinality :db.cardinality/one}
+   })
 
-  {source {target {:feeling feeling}}}
+(defn people-datoms
+  "The datoms listing each character that will be in the
+  simulation. Give a map of keywords to names (strings)."
+  [character-map]
+  (for [[k name] character-map]
+    {:person/id k
+     :person/name name}))
 
-  where source and target are character ids, and feeling is a
-  keyword, :like / :fear / :anger / :none.
+(def feelings-q
+  "Query to pull out all relationship belief entities, parameterised by mind."
+  '[:find (pull ?e [*])
+    :in $ ?mind
+    :where
+    [?e :belief/mind ?mind]
+    [?e :relation/feeling]
+    ])
 
-  The map {:feeling feeling} is known as feelinfo and has metadata for
-  ::sources (), ::because."
-  [characters]
-  (let [target-beliefs {}
-        personal-beliefs (zipmap characters (repeat target-beliefs))]
-    (->
-     (zipmap characters (repeat personal-beliefs))
-     (assoc ::time 0
-            ::characters (set characters)))))
+(defn existing-relation
+  [conn belief]
+  (d/q '[:find (pull ?e [*]) .
+         :in $ [?mind ?x ?y]
+         :where
+         [?e :belief/mind ?mind]
+         [?e :relation/subject ?x]
+         [?e :relation/object ?y]]
+       @conn
+       (map belief [:belief/mind
+                    :relation/subject
+                    :relation/object])))
 
-(defn belief-seq
-  [beliefs]
-  (for [[source m] beliefs
-        [target feelinfo] m]
-    [source target feelinfo]))
+(defn choose-belief-for-gossip
+  [conn speaker listener]
+  (let [bs (d/q feelings-q @conn speaker)]
+    (loop [bs (shuffle bs)]
+      (if-let [belief (first bs)]
+        ;; check whether listener already knows this
+        (let [new-belief (assoc belief :belief/mind listener)
+              their-existing-belief (existing-relation conn new-belief)
+              ]
+          (if (= (:relation/feeling their-existing-belief)
+                 (:relation/feeling new-belief))
+            ;; already known, continue
+            (recur (rest bs))
+            ;; got one
+            new-belief))
+        ;; checked all, no new gossip
+        nil))))
 
-(defn choose-gossip
-  "Character 'speaker' tells another character 'listener' one of their
-  beliefs. It should be something that listener does not already
-  know.
+(defn updatable-relation
+  [conn new-belief]
+  (let [existing (existing-relation conn new-belief)
+        eid (or (:db/id existing) -1)]
+    (assoc new-belief :db/id eid)))
 
-  (?) If the listener already believes something incompatible, then the
-  _later_ belief wins and is communicated back to the speaker. (?)"
-  [state speaker listener]
-  (let [spe-beliefs (get state speaker)
-        lis-beliefs (get state listener)
-        news (set/difference (set (belief-seq spe-beliefs))
-                             (set (belief-seq lis-beliefs)))]
-    (if (seq news)
-      (rand-nth (seq news))
-      nil)))
+(defn believe!
+  [conn mind subject object feeling]
+  (let [belief {:belief/mind mind
+                :relation/subject subject
+                :relation/object object
+                :relation/feeling feeling}
+        em (updatable-relation conn belief)]
+    (d/transact! conn [em])
+    em))
 
-(defn learn
-  [beliefs belief]
-  (let [[source target feelinfo] belief]
-    (assoc-in beliefs [source target] feelinfo)))
+(def dbrules
+  '[[(feels-for ?e ?mind ?subject ?object ?feeling)
+     [?e :belief/mind ?mind]
+     [?e :relation/subject ?subject]
+     [?e :relation/object ?object]
+     [?e :relation/feeling ?feeling]
+     ]
+    ])
 
-(defn felt-for-who
-  [ones-beliefs feeling]
-  (keep (fn [[target info]]
-          (when (= (:feeling info) feeling) target))
-        ones-beliefs))
+;;; ## Responses
 
-;;; ## Rules
+(defn mutual-fear-response
+  [conn mind]
+  (let [ans (d/q '[:find ?x ?e1 ?e2
+                   :in $ ?mind %
+                   :where
+                   (feels-for ?e1 ?mind ?mind ?x :fear)
+                   (feels-for ?e2 ?mind ?x ?mind :fear)
+                   ]
+                 @conn mind dbrules)]
+    (for [[x e1 e2] ans]
+      {:db/id e1
+       :relation/feeling :none
+       :belief/cause e2
+       :belief/source mind
+       :belief/thought
+       (format "Huh? You mean %s is afraid of me?? Well I shouldn't be afraid of them any more!"
+               (name x))})))
 
-(defn dont-fear-each-other
-  [beliefs me]
-  (for [other (felt-for-who (get beliefs me) :fear)
-        :let [their-feelinfo (get-in beliefs [other me])]
-        :when (= :fear (:feeling their-feelinfo))]
-    [me other (with-meta {:feeling :none}
-                {::because [other me their-feelinfo]})]))
+(defn x-like-me-response
+  [conn mind]
+  (let [ans (d/q '[:find ?x ?e1
+                   :in $ ?mind %
+                   :where
+                   (feels-for ?e1 ?mind ?x ?mind :like)
+                   ]
+                 @conn mind dbrules)]
+    (for [[x e1] ans
+          :let [relation {:belief/mind mind
+                          :relation/subject mind
+                          :relation/object x}
+                existing (existing-relation conn relation)]
+          :when (not= :like (:relation/feeling existing))]
+      (assoc relation
+             :db/id (or (:db/id existing) -1)
+             :relation/feeling :like
+             :belief/cause e1
+             :belief/source mind
+             :belief/thought
+             (format "%s likes me! We can be friends."
+                     (name x))))))
 
-(defn like-back
-  [beliefs me]
-  (for [other (keys (dissoc beliefs me))
-        :let [their-feelinfo (get-in beliefs [other me])]
-        :when (= :like (:feeling their-feelinfo))
-        :let [my-feeling (get-in beliefs [me other :feeling])]
-        :when (not= :like my-feeling)]
-    [me other (with-meta {:feeling :like}
-                {::because [other me their-feelinfo]})]))
+(defn x-angry-me-response
+  [conn mind]
+  (let [ans (d/q '[:find ?x ?e1
+                   :in $ ?mind %
+                   :where
+                   (feels-for ?e1 ?mind ?x ?mind :anger)
+                   ]
+                 @conn mind dbrules)]
+    (for [[x e1] ans
+          :let [relation {:belief/mind mind
+                          :relation/subject mind
+                          :relation/object x}
+                existing (existing-relation conn relation)]
+          :when (not (contains? #{:anger :fear} (:relation/feeling existing)))
+          :let [new-feeling (rand-nth [:anger :fear])]]
+      (assoc relation
+             :db/id (or (:db/id existing) -1)
+             :relation/feeling new-feeling
+             :belief/cause e1
+             :belief/source mind
+             :belief/thought
+             (cond
+               (= :anger new-feeling)
+               (format "Well if %s is angry with me then I'm angry too."
+                       (name x))
+               (= :fear new-feeling)
+               (format "Uh-oh, I'd better keep away from %s."
+                       (name x)))))))
 
-(defn anger-response
-  [beliefs me]
-  (for [other (keys (dissoc beliefs me))
-        :let [their-feelinfo (get-in beliefs [other me])]
-        :when (= :anger (:feeling their-feelinfo))
-        :let [my-feeling (get-in beliefs [me other :feeling])]
-        :when (not (contains? #{:anger :fear} my-feeling))
-        :let [new-feeling (rand-nth [:anger :fear])]]
-    [me other (with-meta {:feeling new-feeling}
-                {::because [other me their-feelinfo]})]))
+(defn me-like-x-&-x-like-y-response
+  [conn mind]
+  (let [ans (d/q '[:find ?x ?other ?e2
+                   :in $ ?mind %
+                   :where
+                   (feels-for ?e1 ?mind ?mind ?x :like)
+                   (feels-for ?e2 ?mind ?x ?other :like)
+                   [(not= ?other ?mind)]
+                   ]
+                 @conn mind dbrules)]
+    (for [[x other e2] ans
+          :let [relation {:belief/mind mind
+                          :relation/subject mind
+                          :relation/object other}
+                existing (existing-relation conn relation)]
+          :when (not (contains? #{:anger :like} (:relation/feeling existing)))
+          :let [new-feeling (rand-nth [:anger :like])]]
+      (assoc relation
+             :db/id (or (:db/id existing) -1)
+             :relation/feeling new-feeling
+             :belief/cause e2
+             :belief/source mind
+             :belief/thought
+             (cond
+               (= :anger new-feeling)
+               (format "Oh, why does %1$s like %2$s? I want %1$s to only like me. Now I'm angry with %2$s."
+                       (name x) (name other))
+               (= :like new-feeling)
+               (format "Great, %1$s likes %2$s so they must be fun, we can all hang out together."
+                       (name x) (name other)))))))
 
-(defn jealousy-or-grouping
-  [beliefs me]
-  (for [my-like (felt-for-who (get beliefs me) :like)
-        other (felt-for-who (get beliefs my-like) :like)
-        :when (not= other me)
-        :let [my-feeling (get-in beliefs [me other :feeling])]
-        :when (not (contains? #{:anger :like} my-feeling))
-        :let [new-feeling (rand-nth [:anger :like])]
-        :let [cause [my-like other (get-in beliefs [my-like other])]]]
-    [me other (with-meta {:feeling new-feeling}
-                {::because cause})]))
+(defn me-like-x-&-x-fear-y-response
+  [conn mind]
+  )
 
-(defn loyalty
-  [beliefs me]
-  (for [my-like (felt-for-who (get beliefs me) :like)
-        other (felt-for-who (get beliefs my-like) :fear)
-        :when (not= other me)
-        :let [my-feeling (get-in beliefs [me other :feeling])]
-        :when (not (contains? #{:anger :fear} my-feeling))
-        :let [new-feeling (rand-nth [:anger :fear])]
-        :let [cause [my-like other (get-in beliefs [my-like other])]]]
-    [me other (with-meta {:feeling new-feeling}
-                {::because cause})]))
+(defn me-like-x-&-y-angry-x-response
+  [conn mind]
+  )
 
-(def responses
-  [dont-fear-each-other
-   like-back
-   anger-response
-   jealousy-or-grouping
-   loyalty])
+(defn me-fear-x-&-y-angry-x-response
+  [conn mind]
+  )
 
-;;;; ## end of rules
+(defn me-angry-x-&-y-angry-x-response
+  [conn mind]
+  )
 
-(defn think
-  [beliefs me]
-  (loop [responses responses
-         beliefs beliefs
-         news []]
-    (if-let [r (first responses)]
-      (let [r-news (r beliefs me)]
-        (recur (rest responses)
-               (reduce learn beliefs r-news)
-               (into news r-news)))
-      ;; done
-      [beliefs news])))
+(comment
+  (use 'clojure.pprint)
+  (def conn (d/create-conn db-schema))
+  (believe! conn :fred :fred :ethel :fear)
+  (believe! conn :fred :ethel :fred :fear)
+  (pprint (d/q feelings-q @conn :fred))
+  (d/transact! conn (dont-fear-each-other conn :fred))
+  (pprint (d/q feelings-q @conn :fred))
+)
 
-(defn meet
-  [state speaker listener]
-  (let [gossip (choose-gossip state speaker listener)]
-    (if gossip
-      (let [beliefs (get state listener)
-            new-b (learn beliefs gossip)
-            [new-b-2 news-2] (think new-b listener)
-            [new-b-3 news-3] (think new-b-2 listener)
-            new-state (assoc state listener new-b-3)]
-        [new-state {:speaker speaker
-                    :listener listener
-                    :gossip gossip
-                    :thoughts [news-2 news-3]}])
-      ;; no gossip
-      [state {}])))
 
-(defn random-meeting
-  [state]
-  (let [chars (::characters state)
-        speaker (rand-nth (seq chars))
-        listener (rand-nth (seq (disj chars speaker)))]
-    (meet state speaker listener)))
+(comment
 
-(defn step
-  [state]
-  (let [[new-state stuff] (random-meeting state)]
-    (println stuff)
-    (update new-state
-            ::time inc)))
+  (defn random-meeting
+   [state]
+   (let [chars (::characters state)
+         speaker (rand-nth (seq chars))
+         listener (rand-nth (seq (disj chars speaker)))]
+     (meet state speaker listener))))
+
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
